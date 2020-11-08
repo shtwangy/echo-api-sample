62ff5cc69bbe6fe63a01adcaa9fc6ed41a7be259
shtwangy <shtwangy@gmail.com>
2020-11-08 23:44:57 +0900

    feat(server.go): create post method endpoint.

--- server.go
+++ server.go
@@ -14,11 +14,20 @@ type User struct {
 func main() {
 	e := echo.New()
 	e.GET("/user", show)
+	e.POST("/users", display)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+func display(c echo.Context) error {
 	u := new(User)
 	if err := c.Bind(u); err != nil {
 		return err
