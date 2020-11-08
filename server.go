fbaae87d4d10cbdfb68135f1a8fdd898d9ef49a7
shtwangy <shtwangy@gmail.com>
2020-11-08 23:43:45 +0900

    feat(server.go): create api that receives GET parameters and returns JSON.

--- server.go
+++ server.go
@@ -6,10 +6,22 @@ import (
 	"github.com/labstack/echo"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/user", show)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func show(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, u)
 }
